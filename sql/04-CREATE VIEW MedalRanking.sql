f62f5ab772d9012bce64f6f250308df8ef78db67
Andy Phillips <andy.phillips2@dvla.gov.uk>
2019-07-04 19:34:09 +0100

    New view

---
+++ sql/04-CREATE VIEW MedalRanking.sql
@@ -0,0 +1,17 @@
+USE myDatabase
+GO
+
+CREATE VIEW MedalRanking
+
+AS
+
+SELECT TOP 100 PERCENT 
+	[Country]
+	,[Sport]
+	,SUM(Gold+Silver+Bronze) as Medals
+	,SUM([Gold]) AS Golds
+	,SUM([Silver]) AS Silvers
+	,SUM([Bronze]) as Bronzes
+FROM [dbo].[Athletes]
+GROUP BY [Country],[Sport]
+ORDER BY SUM([Gold]) DESC,SUM([Silver]) DESC, SUM([Bronze]) DESC
